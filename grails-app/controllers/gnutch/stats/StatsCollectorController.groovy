a19f857d2f9cfbbda1dc3d55729f7403eeb4165a
miheretab <mihrtab@gmail.com>
2013-12-11 18:51:57 +0300

    Preparing stats controller index for high chart data

--- grails-app/controllers/gnutch/stats/StatsCollectorController.groovy
+++ grails-app/controllers/gnutch/stats/StatsCollectorController.groovy
@@ -3,10 +3,26 @@ package gnutch.stats
 // Automatic marshalling of XML and JSON
 import grails.converters.*
 
+import org.springframework.beans.factory.annotation.Autowired
 
 class StatsCollectorController {
+	
+	//@Autowired //autowiring is not working for unit testing but it works fine without it
+	StatsCollector statsCollector
+	
+    def index() { 		 
 
-    def index() { 
-      render (stats:[a:1, b:2]) as JSON
+		Map<String, List<Long>> arrayStatistic = statsCollector.getArrayStatistic()
+		 
+		def json = []
+		 
+		int i = 0
+		for (entry in arrayStatistic) {
+			Map m = new HashMap()
+			m.put('name', entry.key)
+			m.put('data', entry.value)
+			json[i++] = m
+		}
+		render json as JSON
     }
 }
---
+++ test/unit/gnutch/stats/StatsCollectorControllerTests.groovy
@@ -0,0 +1,58 @@
+package gnutch.stats
+
+import grails.test.mixin.*
+import grails.test.mixin.support.*
+
+import org.apache.camel.impl.DefaultCamelContext
+import org.apache.camel.impl.DefaultExchange
+import org.junit.*
+import org.quartz.impl.StdSchedulerFactory
+
+/**
+ * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
+ */
+@TestMixin(GrailsUnitTestMixin)
+class StatsCollectorControllerTests {
+
+	def context;
+	def exchange;
+
+	def statsCollector;
+	
+    void setUp() {
+        // Setup logic here
+		context = new DefaultCamelContext();
+		exchange = new DefaultExchange(context);
+	
+		statsCollector = new StatsCollector(statisticTimeoutMsec: 1000); // seeting delay as 1 second
+	
+		statsCollector.statisticTimeoutMsec = 1000;
+		// autowiring field
+		statsCollector.quartzScheduler = new StdSchedulerFactory().getScheduler();
+	
+		statsCollector.quartzScheduler.start();
+	
+	
+		statsCollector.init(); // calling init method
+    }
+
+    void tearDown() {
+        // Tear down logic here
+		statsCollector.quartzScheduler.shutdown();
+    }
+
+    void testIndex() {
+		exchange.in.headers['statsFrom'] = 'abc'
+    	statsCollector.collect(exchange);
+	
+    	Thread.sleep(1000); // waiting for 1 second so job is fired
+		
+		statsCollector.collect(exchange);
+		
+		Thread.sleep(1000); // waiting for 1 second so job is fired
+
+		controller.statsCollector = statsCollector
+		controller.index()
+		assert response.text == '[{"name":"abc","data":[1,0,1]}]';
+    }
+}
