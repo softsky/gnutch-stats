8a077aaaa9fa18a7251f8c64519788327db6b264
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2013-12-24 13:21:16 +0200

    major controller improvement. list() method has been added, index() method now supports JSONP if callback=? parameter is added

--- grails-app/controllers/gnutch/stats/StatsCollectorController.groovy
+++ grails-app/controllers/gnutch/stats/StatsCollectorController.groovy
@@ -14,11 +14,11 @@ class StatsCollectorController {
 		
     if(params.statsFrom) {
 
-      Map<String, List<Long>> arrayStatistic = statsCollector.getArrayStatistic()					 			
-			 
+      Map<String, List<Long>> arrayStatistic = statsCollector.getArrayStatistic()
+
       int i = 0
       for (entry in arrayStatistic) {
-				
+
         if(entry.key == params.statsFrom) {
           Map m = new HashMap()
           m.put('name', entry.key)
@@ -26,6 +26,22 @@ class StatsCollectorController {
           json[i++] = m
         }
       }
+    }
+
+    if(params.callback)
+      render "${params.callback}(${json as JSON})"
+      else
+      render json as JSON
+  }
+
+  def list(){
+    def json = [:]
+    def arrayStatistic = statsCollector.getArrayStatistic()
+
+    json["list"] = []
+    
+    arrayStatistic.each { k, v ->
+      json["list"] << k
     }
     render json as JSON
   }
