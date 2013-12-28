51e6902e1bd395763ebe881e42dc45efd944045d
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2013-12-28 13:13:54 +0200

    tomcat plugin added

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -27,7 +27,7 @@ grails.project.dependency.resolution = {
   }
 
   plugins {
-    build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+    build ":tomcat:$grailsVersion", ':release:2.2.1', ':rest-client-builder:1.0.3', {
       export = false
     }
   }
